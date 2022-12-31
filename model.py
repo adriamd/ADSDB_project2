77e1d445f3ac091b8e3632490a243f28325aa58d
adriamd <50265208+adriamd@users.noreply.github.com>
2022-12-31 18:29:25 +0100

    update models

---
+++ model.py
@@ -0,0 +1,276 @@
+import duckdb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from datetime import datetime as dt
+import os
+from joblib import dump, load
+import xgboost as xgb
+import json
+
+table = "sandbox_T_apartment_S_ga_fl_preprocessed"
+database = 'data/exploitation.db'
+
+# GET TABLE
+
+con = duckdb.connect(database, read_only=True)
+df = con.execute(f"select * from {table}").df()
+df.head()
+con.close()
+
+# ENCODING
+
+def encode(df, scale=False):
+    # select columns
+    y_col = 'price'
+    exclude_cols = ['region']
+    x_cols_onehot = ['type', 'state', 'laundry_options', 'parking_options']
+    x_cols_binary = ['cats_allowed', 'dogs_allowed', 'smoking_allowed', 'wheelchair_access', 'electric_vehicle_charge', 'comes_furnished', 'half_baths'] + [
+        c for c in df.columns if c[0:2]=='b_'
+    ]
+    x_cols_numeric = [c for c in df.columns if not c in [y_col] + exclude_cols + x_cols_onehot + x_cols_binary]
+
+    # binary to numeric
+    bin2num = lambda x: 1 if x=='Yes' or x==1 or x=="Apartment" else 0
+    for col in x_cols_binary:
+        df[col] = df[col].apply(bin2num)
+
+    # one-hot encoding in categorical variables
+    enc = OneHotEncoder(handle_unknown='ignore')
+    enc.fit(df[x_cols_onehot])
+    cols_encoded = []
+    for i in range(len(x_cols_onehot)):
+        col = x_cols_onehot[i]
+        for name in enc.categories_[i]:
+            cols_encoded.append(str(col+'_'+name[0:].strip()).lower().strip())
+    df_onehot_encoded = pd.DataFrame(enc.transform(df[x_cols_onehot]).toarray(), columns = cols_encoded)
+
+    if scale:
+        # scaling numerical variables:
+        scaler = StandardScaler()
+        df_scaled_num = pd.DataFrame(scaler.fit_transform(df[x_cols_numeric]), columns = x_cols_numeric)
+
+        # dataset with encoding + scaling
+        return [
+            pd.concat(objs=[df[x_cols_binary], df_onehot_encoded, df_scaled_num, df[[y_col]]], axis = 1), 
+            {'names':scaler.feature_names_in_, 'means':scaler.mean_, 'scales':scaler.scale_}
+        ]
+
+    # dataset with encoding categorical columns to numeric
+    return pd.concat(objs=[
+        df[x_cols_binary], df_onehot_encoded, df[x_cols_numeric], df[[y_col]]
+    ], axis = 1
+    )
+
+def split_train_test(dff, size_train = 0.7, random_seed=777, y_col='price'):
+    # split into 3 subsets
+    # df_train, df_val, df_test = np.split(
+    #     dff.sample(frac=1, random_state=random_seed),
+    #     [ int(0.6*len(dff)) , int(0.8*len(dff)) ]
+    # )
+
+    # split into 2 subsets
+    df_train, df_test = np.split(
+        dff.sample(frac=1, random_state=random_seed), [ int(size_train*len(dff)) ]
+    )
+
+    return [
+        df_train.drop(y_col, axis=1), # X train
+        df_train[y_col], # y train
+        df_test.drop(y_col, axis=1), # X test
+        df_test[y_col] # y test
+    ]
+
+# ONE FUNCTION FOR EACH ALGORITHM
+
+def printMetrics(y_true, y_pred):
+    print("RMSE: ", round(np.sqrt(metrics.mean_squared_error(y_true, y_pred)), 4))
+    print("MAE: ", round(metrics.mean_absolute_error(y_true, y_pred),4))
+    print("MAPE: ", round(metrics.mean_absolute_percentage_error(y_true, y_pred),4))
+    print("R2: ", round(metrics.r2_score(y_true, y_pred),4))
+
+def scatterplots(y_tr, yhat_tr, y_te, yhat_te, model_name):
+    plt.scatter(y_tr, yhat_tr)
+    plt.title("Training set")
+    plt.xlabel("real value")
+    plt.ylabel("predicted value")
+    plt.savefig("output/"+model_name+"/plot_train.png")
+    plt.clf()
+    plt.scatter(y_te, yhat_te)
+    plt.title("Test set")
+    plt.xlabel("real value")
+    plt.ylabel("predicted value")
+    plt.savefig("output/"+model_name+"/plot_test.png")
+    plt.clf()
+
+
+def linear_regression(X_train, y_train, X_test, y_test, save_plots=True):
+    model_name = "linreg_" + dt.now().strftime("%Y%m%d_%H%M%S")
+
+    reg = LinearRegression().fit(X_train, y_train)
+    yhat_train_lin = reg.predict(X_train)
+    yhat_val_lin = reg.predict(X_test)
+
+    print("Metrics in train set:")
+    printMetrics(y_train, yhat_train_lin)
+    print("Metrics in test set:")
+    printMetrics(y_test, yhat_val_lin)
+
+    # save the model
+    os.mkdir("output/"+model_name)
+    dump(reg, "output/"+model_name+"/linreg.joblib")
+
+    # save the plots
+    if save_plots:
+        scatterplots(y_train, yhat_train_lin, y_test, yhat_val_lin, model_name)
+
+    return reg
+
+# Ridge regression, with loo-cv to choose the regularization parameter
+def ridge_regression(X_train, y_train, X_test, y_test, save_plots=True):
+    model_name = "ridgereg_" + dt.now().strftime("%Y%m%d_%H%M%S")
+
+    rreg = RidgeCV(alphas = np.logspace(-3,3,7)).fit(X_train, y_train)
+    yhat_train_rr = rreg.predict(X_train)
+    yhat_val_rr = rreg.predict(X_test)
+
+    print("Chosen alpha parameter: ", rreg.alpha_)
+    print("Metrics in train set:")
+    printMetrics(y_train, yhat_train_rr)
+    print("Metrics in test set:")
+    printMetrics(y_test, yhat_val_rr)
+
+    os.mkdir("output/"+model_name)
+    dump(rreg, "output/"+model_name+"/ridgereg.joblib")
+
+    if save_plots:
+        scatterplots(y_train, yhat_train_rr, y_test, yhat_val_rr, model_name)
+
+    return rreg
+
+
+# Lasso regression, with loo-cv to choose the regularization parameter
+def lasso_regression(X_train, y_train, X_test, y_test, save_plots=True):
+    model_name = "lassoreg_" + dt.now().strftime("%Y%m%d_%H%M%S")
+
+    lreg = LassoCV(alphas = np.logspace(-3,3,7)).fit(X_train, y_train)
+    yhat_train_lr = lreg.predict(X_train)
+    yhat_val_lr = lreg.predict(X_test)
+
+    print("Chosen alpha parameter: ", lreg.alpha_)
+    print("Metrics in train set:")
+    printMetrics(y_train, yhat_train_lr)
+    print("Metrics in test set:")
+    printMetrics(y_test, yhat_val_lr)
+
+    os.mkdir("output/"+model_name)
+    dump(lreg, "output/"+model_name+"/lassoreg.joblib")
+
+    if save_plots:
+        scatterplots(y_train, yhat_train_lr, y_test, yhat_val_lr, model_name)
+
+    return lreg
+
+def random_forest(X_train, y_train, X_test, y_test, save_plots=True):
+    model_name = "randomforest_" + dt.now().strftime("%Y%m%d_%H%M%S")
+
+    rf = RandomForestRegressor(oob_score=True).fit(X_train, y_train)
+    yhat_train_rf = rf.predict(X_train)
+    yhat_val_rf = rf.predict(X_test)
+
+    print("Metrics in train set:")
+    printMetrics(y_train, yhat_train_rf)
+    print("Metrics in test set:")
+    printMetrics(y_test, yhat_val_rf)
+
+    os.mkdir("output/"+model_name)
+    dump(rf, "output/"+model_name+"/randomforest.joblib")
+
+    if save_plots:
+        scatterplots(y_train, yhat_train_rf, y_test, yhat_val_rf, model_name)
+        # top 10 feature importance
+        sorted_idx = rf.feature_importances_.argsort()
+        plt.figure(figsize=(10,10))
+        plt.barh(rf.feature_names_in_[sorted_idx][-20:], rf.feature_importances_[sorted_idx][-20:])
+        plt.xticks(fontsize=10)
+        plt.yticks(fontsize=10)
+        plt.savefig("output/"+model_name+"/feature_importance_top20.png")
+        plt.clf()
+
+# XGBoost, without crossvalidation. Uses subsampling to avoid overfitting
+def xgboost_noCV(X_train, y_train, X_test, y_test, save_plots=True,
+    params = {
+        'objective':'reg:squarederror',
+        'verbosity':1,
+        'learning_rate':0.3,
+        'max_depth':10,
+        'subsample':0.8, # to avoid overfitting
+        'reg_lambda':0.1, # L2 reg
+        'alpha':0, #L1 reg
+        'max_leaves':10
+    }, n_estim=20):
+
+    model_name = "xgb_" + dt.now().strftime("%Y%m%d_%H%M%S")
+
+    data_train = xgb.DMatrix(X_train, label=y_train)
+    data_val = xgb.DMatrix(X_test, label=y_test)
+    xgb_1 = xgb.train(params, data_train, n_estim)
+    yhat_train_xgb1 = xgb_1.predict(data_train)
+    yhat_val_xgb1 = xgb_1.predict(data_val)
+
+    print("Metrics in train set:")
+    printMetrics(y_train, yhat_train_xgb1)
+    print("Metrics in test set:")
+    printMetrics(y_test, yhat_val_xgb1)
+
+    os.mkdir("output/"+model_name)
+    dump(xgb_1, "output/"+model_name+"/xgb.joblib")
+    with open("output/"+model_name+"/xgb_params.json", "w") as fp:
+        params['n_estim'] = n_estim
+        json.dump(params, fp)
+
+    if save_plots:
+        scatterplots(y_train, yhat_train_xgb1, y_test, yhat_val_xgb1, model_name)
+        xgb.plot_importance(xgb_1, max_num_features = 20, height = 1)
+        fig = plt.gcf()
+        fig.savefig("output/"+model_name+"/feature_importance_top20.png")
+        plt.clf()
+        try:
+            xgb.plot_tree(xgb_1, num_trees = 4, rankdir="LR")
+            fig = plt.gcf()
+            fig.savefig("output/"+model_name+"/tree.png")
+            plt.clf()
+        except:
+            pass
+
+
+
+
+
+
+
+
+def model_1():
+    con = duckdb.connect(database, read_only=True)
+    df = con.execute(f"select * from {table}").df()
+    con.close()
+
+    df_encoded = encode(df, scale=False)
+    df_scaled, scale_params = encode(df, scale=True)
+    # save the scale params in a file?
+
+    X_train, y_train, X_test, y_test = split_train_test(df_encoded)
+    Xs_train, ys_train, Xs_test, ys_test = split_train_test(df_scaled)
+
+
+        """{'alpha': 0,
+ 'learning_rate': 0.3,
+ 'max_depth': 10,
+ 'max_leaves': 10,
+ 'n_estimators': 20,
+ 'reg_lambda': 0,
+ 'subsample': 0.8}"""
