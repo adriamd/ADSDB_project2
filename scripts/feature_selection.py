310a16cbc67da1e29d9ec01de39da6a38b23cc2e
Eloi Campeny <eloi.campeny@estudiantat.upc.edu>
2023-01-13 16:52:04 +0100

    feature selection modified

--- scripts/feature_selection.py
+++ scripts/feature_selection.py
@@ -4,52 +4,8 @@ import numpy as np
 
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.ensemble import RandomForestRegressor
-
-
 from skfeature.function.information_theoretical_based.CIFE import cife
-# ENCODING
 
-def encode(df, scale=False):
-    # select columns
-    y_col = 'price'
-    exclude_cols = ['region']
-    x_cols_onehot = ['type', 'state', 'laundry_options', 'parking_options']
-    x_cols_binary = ['cats_allowed', 'dogs_allowed', 'smoking_allowed', 'wheelchair_access', 'electric_vehicle_charge', 'comes_furnished', 'half_baths'] + [
-        c for c in df.columns if c[0:2]=='b_'
-    ]
-    x_cols_numeric = [c for c in df.columns if not c in [y_col] + exclude_cols + x_cols_onehot + x_cols_binary]
-
-    # binary to numeric
-    bin2num = lambda x: 1 if x=='Yes' or x==1 or x=="Apartment" else 0
-    for col in x_cols_binary:
-        df[col] = df[col].apply(bin2num)
-
-    # one-hot encoding in categorical variables
-    enc = OneHotEncoder(handle_unknown='ignore')
-    enc.fit(df[x_cols_onehot])
-    cols_encoded = []
-    for i in range(len(x_cols_onehot)):
-        col = x_cols_onehot[i]
-        for name in enc.categories_[i]:
-            cols_encoded.append(str(col+' '+name[0:].strip()).lower().strip())
-    df_onehot_encoded = pd.DataFrame(enc.transform(df[x_cols_onehot]).toarray(), columns = cols_encoded)
-
-    if scale:
-        # scaling numerical variables:
-        scaler = StandardScaler()
-        df_scaled_num = pd.DataFrame(scaler.fit_transform(df[x_cols_numeric]), columns = x_cols_numeric)
-
-        # dataset with encoding + scaling
-        return [
-            pd.concat(objs=[df[x_cols_binary], df_onehot_encoded, df_scaled_num, df[[y_col]]], axis = 1), 
-            {'names':scaler.feature_names_in_, 'means':scaler.mean_, 'scales':scaler.scale_}
-        ]
-
-    # dataset with encoding categorical columns to numeric
-    return pd.concat(objs=[
-        df[x_cols_binary], df_onehot_encoded, df[x_cols_numeric], df[[y_col]]
-    ], axis = 1
-    )
 
 def feature_cife(df,target):
 
@@ -77,22 +33,41 @@ def feature_selection_random_forest(df,target):
 
 
 if __name__ == "__main__":
-    table = "sandbox_T_apartment_S_ca_preprocessed"
+    table = "sandbox_T_apartment_S_ca"
     database = 'data/exploitation.db'
     target = "price"
+    scaled = False
 
     con = duckdb.connect(database, read_only=True)
-    df = con.execute(f"select * from {table}").df()
+
+    if scaled:
+        X_train = con.execute(f"select * from {table}_Xs_train_processed").df()
+        y_train = con.execute(f"select * from {table}_ys_train_processed").df()
+        X_test = con.execute(f"select * from {table}_Xs_test_processed").df()
+    else:
+        X_train = con.execute(f"select * from {table}_X_train_processed").df()
+        y_train = con.execute(f"select * from {table}_y_train_processed").df()
+        X_test = con.execute(f"select * from {table}_X_test_processed").df()
     con.close()
+
+    X_train.reset_index(drop=True, inplace=True)
+    y_train.reset_index(drop=True, inplace=True)
+
+    df = pd.concat(objs=[X_train, y_train ], axis = 1)
     
-    df_encoded = encode(df, scale=False)
-    df_sample = df_encoded.sample(round(len(df)*0.05), random_state=777)
+    df_sample = df.sample(round(len(df)*0.05), random_state=777)
     
     columns = feature_selection_random_forest(df_sample,target)
-    columns.append(target)
     
-    df_reduced = df_encoded[columns]
+    X_train_reduced = X_train[columns]
+    X_test_reduced = X_test[columns]
+
     con = duckdb.connect(database, read_only=False)
-    out_table = table[:-13]
-    con.execute(f"CREATE OR REPLACE TABLE {out_table}_reduced AS SELECT * FROM df_reduced")
+
+    if scaled:
+        con.execute(f"CREATE OR REPLACE TABLE {table}_Xs_train_reduced AS SELECT * FROM X_train_reduced")
+        con.execute(f"CREATE OR REPLACE TABLE {table}_Xs_test_reduced AS SELECT * FROM X_test_reduced")
+    else:
+        con.execute(f"CREATE OR REPLACE TABLE {table}_X_train_reduced AS SELECT * FROM X_train_reduced")
+        con.execute(f"CREATE OR REPLACE TABLE {table}_X_test_reduced AS SELECT * FROM X_test_reduced")
     con.close()
